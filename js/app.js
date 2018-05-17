f7afdd9362ad5e92f46f5de13852684e2ce4db41
Alan Wood <woodevadev@gmail.com>
2018-05-17 19:42:24 -0400

    Fixed some stuff

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //Selects the table definitions
-let $tableSelector = $('table td');
+let $liSelector = $('ul li');
 
 $('#overlay').hide();
 $('#modal').hide();
@@ -24,22 +24,36 @@ $.ajax({
             theHTML += `<p>${data.results[i].location.street}, ${data.results[i].location.state} ${data.results[i].location.postcode}</p>`;
             theHTML += `<p>Birthday: ${data.results[i].dob}</p>`;
             theHTML += `</div>`;
-            $($tableSelector[i]).html(theHTML);
+            $($liSelector[i]).html(theHTML);
             $('.hidden').hide();
             theHTML = '';
         }
     }
 });
 
-function centerModal(){
-    
-}
+//Finds the appropriate top and left values
+//to center the modal on the screen
+let thetop = Math.max($(window).height() - $('#modal').outerHeight(), 0) / 2;
+let theleft = Math.max($(window).width() - $('#modal').outerWidth(), 0) / 2;
 
-$('td').on('click',function(event){
+//Places the modal in the middle of the screen
+$('#modal').css('top', thetop + $(window).scrollTop());
+$('#modal').css('left', theleft + $(window).scrollLeft());
+
+//The X for exiting the modal
+let $theX = ('<h1>X</h1>');
+
+//Select content div and append the X
+let $contentDiv = $('.content');
+$contentDiv.append($theX);
+
+$('.box').on('click',function(event){
+    let $theImage = $(event.target).children('.image');
+    console.log($theImage);
     
-    
-    $('#overlay').show();
-    $('#modal').show();
+
+    // $('#overlay').show();
+    // $('#modal').show();
 });
 
 
